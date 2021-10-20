8d29139a7785c024a836dbf5e8c838e686c11bdc
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-20 16:16:37 -0600

    Add Animal

--- object-examples.js
+++ object-examples.js
@@ -1,1 +1,2 @@
 var newCar = {color: 'Red', topSpeed: 150, isCool: true, specialFeatures: [Sunroof, CD Player, Air Conditioning]}
+var animal = {breed: 'Lion', legs: 4, is Dangerous: true, specialFacts: [Massive, Smelly, Sleepy during the day]}
