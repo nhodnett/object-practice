cd5adf1421b918256cda58b123f25c477e4e596b
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-20 16:19:30 -0600

    Add Customer

--- object-examples.js
+++ object-examples.js
@@ -1,2 +1,3 @@
 var newCar = {color: 'Red', topSpeed: 150, isCool: true, specialFeatures: [Sunroof, CD Player, Air Conditioning]}
-var animal = {breed: 'Lion', legs: 4, is Dangerous: true, specialFacts: [Massive, Smelly, Sleepy during the day]}
+var animal = {breed: 'Lion', legs: 4, isDangerous: true, specialFacts: [Massive, Smelly, Sleepy during the day]}
+var customer = {name: 'Karen', age: 35, isFriendly: false, specialtraits: [Wants to see the Manager]}
