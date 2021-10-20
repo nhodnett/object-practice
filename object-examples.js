e3581892186aa81cc789d8f11dc0fd2558ba8517
Nathan Hodnett <nathanhodnett@gmail.com>
2021-10-20 16:31:39 -0600

    Made Syntax corrections

--- object-examples.js
+++ object-examples.js
@@ -1,3 +1,3 @@
-var newCar = {color: 'Red', topSpeed: 150, isCool: true, specialFeatures: [Sunroof, CD Player, Air Conditioning]}
-var animal = {breed: 'Lion', legs: 4, isDangerous: true, specialFacts: [Massive, Smelly, Sleepy during the day]}
-var customer = {name: 'Karen', age: 35, isFriendly: false, specialtraits: [Wants to see the Manager]}
+var newCar = {color: 'Red', topSpeed: 150, isCool: true, specialFeatures: ['Sunroof', 'CD Player', 'Air Conditioning']}
+var animal = {breed: 'Lion', legs: 4, isDangerous: true, specialFacts: ['Massive', 'Smelly', 'Sleepy during the day']}
+var customer = {name: 'Karen', age: 35, isFriendly: false, specialtraits: ['Wants to see the Manager']}
